941159c08c7b6b5cec09bb233fc77907d1543b9a
P-JIANGH <18304030383@163.com>
2018-06-05 14:58:02 +0800

    add mode partation

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,28 @@ const path = require('path');
 const prod = require('./enviorment/enviorment.prod');
 
 module.exports = {
-    mode: prod ? 'production' : 'development',
-    entry: './src/index.js',
+    mode: prod ? 'production' : 'development',  // process.env.NODE_ENV
+    devtool: prod ? 'eval-source-map' : 'eval',
+    entry: {
+        main: './src/index.js',
+    },
     output: {
-        filename: 'index.bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
     module: {
         rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/, 
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env'],
+                    }
+            } },
             { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+            { test: /\.san$/, loader: 'san-loader' },
         ],
     }
 }
