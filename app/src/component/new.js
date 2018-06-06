a19dce60ccb6fead990e1eaec1cc60accd89eb4c
P-JIANGH <18304030383@163.com>
2018-06-06 13:38:42 +0800

    finished ife mvvm 2.1!

--- app/src/component/new.js
+++ app/src/component/new.js
@@ -1,13 +1,44 @@
-import san from 'san';
+import san, { DataTypes } from 'san';
 
 var newApp = san.defineComponent({
-    template: `<p>Hello {{name}} from P-JIANGH</p>`,
+  template: `
+    <div>
+      <div>
+        <input type="text" placeholder="姓名（string）" value="{= name =}" />
+        <input type="text" placeholder="年龄（number）" value="{= age =}" />
+        <input type="text" placeholder="简介（string）" value="{= des =}" />
+      </div>
+      <div style="display: inline-block; width: 200px;">
+        <div style="margin: 20px 0;">
+        信息：<button on-click="reset" style="margin-left: 60px;">移除信息</button>
+        </div>
+        <div>姓名：{{name || '________'}}</div>
+        <div>年龄：{{age || '________'}}</div>
+        <div>简介：{{des || '________'}}</div>
+      </div>
+      
+    </div>
+  `,
+  dataTypes: {
+    name: DataTypes.string,
+    age: DataTypes.number,
+    des: DataTypes.string,
+  },
 
-    initData: function () {
-        return {
-            name: 'san',
-        };
-    }
+  initData: function () {
+    return {
+      name: null,
+      age: null,
+      des: null,
+    };
+  },
+
+  // 处理点击事件，重置输入
+  reset() {
+    this.data.set('name', null);
+    this.data.set('age', null);
+    this.data.set('des', null);
+  }
 });
 
 export default newApp;
--- webpack.config.js
+++ webpack.config.js
@@ -4,53 +4,54 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
-    devtool: process.env.NODE_ENV === 'production' ? 'source-map' : 'eval',
-    entry: {
-        main: './app/index.js',
-    },
-    devServer: {
-        contentBase: './dist',
-        index: 'index.html',
-        open: true,
-        port: 9000,
-        hot: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/, 
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['babel-preset-env'],
-                        plugins: ['babel-plugin-transform-runtime'],
-                    }
-                }
-            },
-            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-            { test: /\.san$/, loader: 'san-loader' },
-        ],
-    },
-    plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new HtmlWebpackPlugin({
-            title: 'San Hello World!',
-            filename: 'index.html',
-        }),
-        new webpack.NamedModulesPlugin(),
-        new webpack.HotModuleReplacementPlugin()
-    ],
-    output: {
-        filename: '[name].bundle.js',
-        chunkFilename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    resolve: {
-        alias: {
-            san: process.env.NODE_ENV === 'production'
-                ? 'san/dist/san.js'
-                : 'san/dist/san.dev.js'
+  devtool: process.env.NODE_ENV === 'production' ? 'source-map' : 'eval',
+  entry: {
+    main: './app/index.js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    index: 'index.html',
+    clientLogLevel: 'warning',
+    open: true,
+    port: 9000,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/, 
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-env'],
+            plugins: ['babel-plugin-transform-runtime'],
+          }
         }
+      },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      { test: /\.san$/, loader: 'san-loader' },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'San Hello World!',
+      filename: 'index.html',
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    alias: {
+      san: process.env.NODE_ENV === 'production'
+        ? 'san/dist/san.js'
+        : 'san/dist/san.dev.js'
     }
+  }
 };
